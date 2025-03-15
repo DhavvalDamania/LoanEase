3d9c47767cc3e832b524870075d43c47940c8d92
Janav Gandhi <gandhijanav@gmail.com>
2025-03-15 19:58:51 +0530

    Update sarvam_translate.py

--- sarvam_translate.py
+++ sarvam_translate.py
@@ -2,7 +2,7 @@ import requests
 
 url = "https://api.sarvam.ai/translate"
 
-<<<<<<< Updated upstream
+
 def translation(input_string,source_lang,target_lang):
     payload = {
         "input": input_string,
@@ -15,32 +15,7 @@ def translation(input_string,source_lang,target_lang):
         "output_script": "fully-native",
         "numerals_format": "international"
     }
-=======
-source_language_code = input("Enter the source language code (e.g., 'en-IN'): ")
-target_language_code = input("Enter the target language code (e.g., 'gu-IN'): ")    
-input_text = input("Enter the text you want to translate: ")
 
-payload = {
-    "input": input_text,
-    "source_language_code": source_language_code,
-    "target_language_code": target_language_code,
-    "speaker_gender": "Female",
-    "mode": "formal",
-    "model": "mayura:v1",
-    "enable_preprocessing": False,
-    "output_script": "fully-native",
-    "numerals_format": "international"
-}
-
-headers = {
-    "api-subscription-key": "723d3b71-6d58-48ce-a1fe-eea9819daafb",
-    "Content-Type": "application/json"
-}
-
-response = requests.request("POST", url, json=payload, headers=headers)
-
-print(response.text)
->>>>>>> Stashed changes
 
     headers = {
         "api-subscription-key": "723d3b71-6d58-48ce-a1fe-eea9819daafb",
@@ -51,4 +26,3 @@ print(response.text)
 
     print(response.text)
 
-#hi
