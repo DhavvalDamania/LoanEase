054ff2f07c21c9531aa4138da82eea9b6ba7b216
Janav Gandhi <gandhijanav@gmail.com>
2025-03-15 13:53:57 +0530

    Create sarvam_translate.py

---
+++ sarvam_translate.py
@@ -0,0 +1,24 @@
+import requests
+
+url = "https://api.sarvam.ai/translate"
+
+payload = {
+    "input": "hello how are you",
+    "source_language_code": "en-IN",
+    "target_language_code": "gu-IN",
+    "speaker_gender": "Female",
+    "mode": "formal",
+    "model": "mayura:v1",
+    "enable_preprocessing": False,
+    "output_script": "roman",
+    "numerals_format": "international"
+}
+
+headers = {
+    "api-subscription-key": "723d3b71-6d58-48ce-a1fe-eea9819daafb",
+    "Content-Type": "application/json"
+}
+
+response = requests.request("POST", url, json=payload, headers=headers)
+
+print(response.text)
